505e552fee65a6199218e8c2d8b48c51586da400
Gabotero <gabri22_@hotmail.com>
2013-12-07 13:45:00 +0100

    first commit

---
+++ asterisk11/include/asterisk/build.h
@@ -0,0 +1,11 @@
+/*
+ * build.h 
+ * Automatically generated
+ */
+#define BUILD_HOSTNAME "gabriel-laptop"
+#define BUILD_KERNEL "2.6.32-45-generic"
+#define BUILD_MACHINE "x86_64"
+#define BUILD_OS "Linux"
+#define BUILD_DATE "2013-11-11 22:51:51 UTC"
+#define BUILD_USER "gabriel"
+
